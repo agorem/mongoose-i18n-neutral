c46d2dceee22d1335d536a0dfd0ef65aa954e252
Nikita Bayev <nikita@bayev.kz>
2018-03-26 12:02:09 +0600

    Rewrite tests

--- tests/helpers/DummyHelper.js
+++ tests/helpers/DummyHelper.js
@@ -1,23 +1,23 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
-var DummyHelper = {
-  getI18nValidModel: function (modelName) {
-    var schema = new mongoose.Schema({
+const DummyHelper = {
+  getI18nValidModel(modelName) {
+    const schema = new mongoose.Schema({
       name: {
         type: String,
         i18n: true
       }
     });
-    return mongoose.model(modelName,schema);
+    return mongoose.model(modelName, schema);
   },
-  getI18nInvalidModel: function (modelName) {
-    var schema = new mongoose.Schema({
+  getI18nInvalidModel(modelName) {
+    const schema = new mongoose.Schema({
       name: {
         type: Number,
         i18n: true
       }
     });
-    return mongoose.model(modelName,schema);
+    return mongoose.model(modelName, schema);
   }
 };
 
--- tests/test-model-docs.js
+++ tests/test-model-docs.js
@@ -1,36 +1,41 @@
-var mongoose = require('mongoose');
-var mongooseI18nNeutral = require('../index');
+const mongoose = require('mongoose');
+const mongooseI18nNeutral = require('../index');
 
-var langs = ['es','fr','en'];
-mongoose.plugin(mongooseI18nNeutral, {languages: langs});
+const languages = ['es', 'fr', 'en'];
+mongoose.plugin(mongooseI18nNeutral, { languages });
 
-var chai = require('chai');
-var expect = chai.expect;
+const chai = require('chai');
+const expect = chai.expect;
 
-var DummyHelper = require('./helpers/DummyHelper');
+const DummyHelper = require('./helpers/DummyHelper');
 
-describe('Default usage', function() {
-  it('Valid I18n model', function() {
-    var testModel = DummyHelper.getI18nValidModel('demo-i18n-ok');
-    expect(testModel.getLanguages()).to.deep.equal(langs);
+describe('Default usage', () => {
+  it('Valid i18n model', () => {
+    const testModel = DummyHelper.getI18nValidModel('demo-i18n-ok');
+    expect(testModel.getLanguages()).to.deep.equal(languages);
 
-    var doc = new testModel();
-    expect(doc.getLanguages()).to.deep.equal(langs);
+    const doc = new testModel();
+    expect(doc.getLanguages()).to.deep.equal(languages);
+
     doc.name.default = 'hello';
     doc.name.es = 'hola';
     doc.name.fr = 'salut';
+
     expect(doc.name._def).to.equal('hello');
     expect(doc.name.es).to.equal('hola');
     expect(doc.name.fr).to.equal('salut');
     expect(doc.name).to.have.property('en');
     expect(doc.name.default).to.equal('hello');
-    expect(doc.name.i18n).to.include({_def: 'hello', es: 'hola', fr: 'salut'});
-
+    expect(doc.name.i18n).to.include({
+      _def: 'hello',
+      es: 'hola',
+      fr: 'salut'
+    });
   });
-  it('Invalid I18n model', function() {
-    expect(function() {
-      var model = DummyHelper.getI18nInvalidModel('demo-i18n-nok');
+
+  it('Invalid i18n model', () => {
+    expect(() => {
+      const model = DummyHelper.getI18nInvalidModel('demo-i18n-nok');
     }).to.throw('mongoose-i18n-neutral plugin applies only to Strings');
   });
-
 });
